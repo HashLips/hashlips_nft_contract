9485457b04795a4a29105e7ffc076dd57c256e4f
HashLips <88177839+HashLips@users.noreply.github.com>
2021-09-28 17:19:35 +0200

    Added starter files

---
+++ README.md
@@ -0,0 +1,40 @@
+# Welcome to HashLips 👄
+
+![](https://github.com/HashLips/hashlips_nft_contract/blob/main/logo.png)
+
+All the code in these repos was created and explained by HashLips on the main YouTube channel.
+
+To find out more please visit:
+
+[📺 YouTube](https://www.youtube.com/channel/UC1LV4_VQGBJHTJjEWUmy8nA)
+
+[👄 Discord](https://discord.com/invite/qh6MWhMJDN)
+
+[💬 Telegram](https://t.me/hashlipsnft)
+
+[🐦 Twitter](https://twitter.com/hashlipsnft)
+
+[ℹ️ Website](https://hashlips.online/HashLips)
+
+# HashLips NFT contract 🔥
+
+![](https://github.com/HashLips/hashlips_nft_contract/blob/main/banner.png)
+
+This repo provides a simple NFT smart contract.
+
+## Usage ℹ️
+
+To make use of the NFT smart contract, simply copy the `SimpleNft_flat.sol` file in the `contract` folder and deploy it on remix after changing the parameters.
+
+## Disclaimer
+
+    These contracts have been used to create tutorials,
+    and was created for the purpose to teach people
+    how to create smart contracts on the blockchain.
+    please review this code on your own before using any of
+    the following code for production.
+    HashLips will not be liable in any way if for the use
+    of the code. That being said, the code has been tested
+    to the best of the developers' knowledge to work as intended.
+
+That's it! you're done.
---
+++ contract/SimpleNft.sol
@@ -0,0 +1,133 @@
+// SPDX-License-Identifier: MIT
+
+// Amended by HashLips
+/**
+    !Disclaimer!
+    These contracts have been used to create tutorials,
+    and was created for the purpose to teach people
+    how to create smart contracts on the blockchain.
+    please review this code on your own before using any of
+    the following code for production.
+    HashLips will not be liable in any way if for the use 
+    of the code. That being said, the code has been tested 
+    to the best of the developers' knowledge to work as intended.
+*/
+
+pragma solidity >=0.7.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721Enumerable, Ownable {
+  using Strings for uint256;
+
+  string baseURI;
+  string public baseExtension = ".json";
+  uint256 public cost = 0.05 ether;
+  uint256 public maxSupply = 10000;
+  uint256 public maxMintAmount = 20;
+  bool public paused = false;
+  bool public revealed = false;
+  string public notRevealedUri;
+
+  constructor(
+    string memory _name,
+    string memory _symbol,
+    string memory _initBaseURI,
+    string memory _initNotRevealedUri
+  ) ERC721(_name, _symbol) {
+    setBaseURI(_initBaseURI);
+    setNotRevealedURI(_initNotRevealedUri);
+  }
+
+  // internal
+  function _baseURI() internal view virtual override returns (string memory) {
+    return baseURI;
+  }
+
+  // public
+  function mint(uint256 _mintAmount) public payable {
+    uint256 supply = totalSupply();
+    require(!paused);
+    require(_mintAmount > 0);
+    require(_mintAmount <= maxMintAmount);
+    require(supply + _mintAmount <= maxSupply);
+
+    if (msg.sender != owner()) {
+      require(msg.value >= cost * _mintAmount);
+    }
+
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+      _safeMint(msg.sender, supply + i);
+    }
+  }
+
+  function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    for (uint256 i; i < ownerTokenCount; i++) {
+      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    }
+    return tokenIds;
+  }
+
+  function tokenURI(uint256 tokenId)
+    public
+    view
+    virtual
+    override
+    returns (string memory)
+  {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+    
+    if(revealed == false) {
+        return notRevealedUri;
+    }
+
+    string memory currentBaseURI = _baseURI();
+    return bytes(currentBaseURI).length > 0
+        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+        : "";
+  }
+
+  //only owner
+  function reveal() public onlyOwner() {
+      revealed = true;
+  }
+  
+  function setCost(uint256 _newCost) public onlyOwner() {
+    cost = _newCost;
+  }
+
+  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner() {
+    maxMintAmount = _newmaxMintAmount;
+  }
+  
+  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
+    notRevealedUri = _notRevealedURI;
+  }
+
+  function setBaseURI(string memory _newBaseURI) public onlyOwner {
+    baseURI = _newBaseURI;
+  }
+
+  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+    baseExtension = _newBaseExtension;
+  }
+
+  function pause(bool _state) public onlyOwner {
+    paused = _state;
+  }
+ 
+  function withdraw() public payable onlyOwner {
+    (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+    require(success);
+  }
+}
