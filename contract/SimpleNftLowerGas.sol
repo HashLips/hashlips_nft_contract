d091e2566b0a02b651bc9896b664e306d143dee3
Marco Lipparini <developer@liarco.net>
2022-01-06 23:19:36 +0100

    Updating contract code after live tests

--- contract/SimpleNftLowerGas.sol
+++ contract/SimpleNftLowerGas.sol
@@ -8,7 +8,7 @@
     how to create smart contracts on the blockchain.
     please review this code on your own before using any of
     the following code for production.
-    HashLips will not be liable in any way if for the use 
+    HashLips will not be liable in any way for the use 
     of the code. That being said, the code has been tested 
     to the best of the developers' knowledge to work as intended.
 */
@@ -19,76 +19,46 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// [BEGIN] OpenSea interfaces (see: https://github.com/ProjectOpenSea/opensea-creatures/blob/master/contracts/ERC721Tradable.sol)
-contract OwnableDelegateProxy {}
-
-/**
- * Used to delegate ownership of a contract to another address, to save on unneeded transactions to approve contract use for users
- */
-contract ProxyRegistry {
-    mapping(address => OwnableDelegateProxy) public proxies;
-}
-// [END] OpenSea interfaces
-
-contract NFT is ERC721, Ownable {
+contract SimpleNftLowerGas is ERC721, Ownable {
   using Strings for uint256;
-
-  string baseURI;
-  string public baseExtension = ".json";
-  uint256 public cost = 0.05 ether;
-  /*
-   * Always set this to "MAX_SUPPLY + 1", this allows to use a more
-   * effcient counter and have lower gas fees.
-   */
-  uint256 public maxSupply = 10000 + 1; 
-  uint256 public maxMintAmount = 20;
-  bool public paused = false;
-  bool public revealed = false;
-  string public notRevealedUri;
-  /*
-   * Using Counters instead of ETC721Enumerable to save gas.
-   * See https://shiny.mirror.xyz/OUampBbIz9ebEicfGnQf5At_ReMHlZy0tB4glb9xQ0E
-   */
   using Counters for Counters.Counter;
-  Counters.Counter private _nextTokenId;
-  address openSeaProxyRegistryAddress;
 
-  constructor(
-    string memory _name,
-    string memory _symbol,
-    string memory _initBaseURI,
-    string memory _initNotRevealedUri,
-    address _openSeaProxyRegistryAddress
-  ) ERC721(_name, _symbol) {
-    setBaseURI(_initBaseURI);
-    setNotRevealedURI(_initNotRevealedUri);
+  Counters.Counter private supply;
 
-    // Avoid higher gas fee for the first minter by initializing the counter here.
-    _nextTokenId.increment();
-    openSeaProxyRegistryAddress = _openSeaProxyRegistryAddress;
+  string public uriPrefix = "";
+  string public uriSuffix = ".json";
+  string public hiddenMetadataUri;
+  
+  uint256 public cost = 0.01 ether;
+  uint256 public maxSupply = 10000;
+  uint256 public maxMintAmountPerTx = 5;
+
+  bool public paused = true;
+  bool public revealed = false;
+
+  constructor() ERC721("NAME", "SYMBOL") {
+    setHiddenMetadataUri("ipfs://__CID__/hidden.json");
   }
 
-  // internal
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseURI;
+  modifier mintCompliance(uint256 _mintAmount) {
+    require(_mintAmount > 0 && _mintAmount <= maxMintAmountPerTx, "Invalid mint amount!");
+    require(supply.current() + _mintAmount <= maxSupply, "Max supply exceeded!");
+    _;
   }
 
-  // public
-  function mint(uint256 _mintAmount) public payable {
-    uint256 nextId = _nextTokenId.current();
-    require(!paused);
-    require(_mintAmount > 0);
-    require(_mintAmount <= maxMintAmount);
-    require(nextId + _mintAmount <= maxSupply);
+  function totalSupply() public view returns (uint256) {
+    return supply.current();
+  }
 
-    if (msg.sender != owner()) {
-      require(msg.value >= cost * _mintAmount);
-    }
+  function mint(uint256 _mintAmount) public payable mintCompliance(_mintAmount) {
+    require(!paused, "The contract is paused!");
+    require(msg.value >= cost * _mintAmount, "Insufficient funds!");
 
-    for (uint256 i = 1; i <= _mintAmount; i++) {
-      _safeMint(msg.sender, _nextTokenId.current());
-      _nextTokenId.increment();
-    }
+    _mintLoop(msg.sender, _mintAmount);
+  }
+  
+  function mintForAddress(uint256 _mintAmount, address _receiver) public mintCompliance(_mintAmount) onlyOwner {
+    _mintLoop(_receiver, _mintAmount);
   }
 
   function walletOfOwner(address _owner)
@@ -97,27 +67,26 @@ contract NFT is ERC721, Ownable {
     returns (uint256[] memory)
   {
     uint256 ownerTokenCount = balanceOf(_owner);
-    uint256[] memory ownedTokens = new uint256[](ownerTokenCount);
-    uint256 currentTokenIndex = 1; // loop through all tokens from 1 to (maxSupply - 1)
-    uint256 ownedTokenIndex = 0; // index for returned array
+    uint256[] memory ownedTokenIds = new uint256[](ownerTokenCount);
+    uint256 currentTokenId = 1;
+    uint256 ownedTokenIndex = 0;
 
-    // Early return if all tokens owned by this address have been found
-    while (ownedTokenIndex < ownerTokenCount && currentTokenIndex < maxSupply) {
-      address currentTokenOwner = ownerOf(currentTokenIndex);
-      
+    while (ownedTokenIndex < ownerTokenCount && currentTokenId <= maxSupply) {
+      address currentTokenOwner = ownerOf(currentTokenId);
+
       if (currentTokenOwner == _owner) {
-        ownedTokens[ownedTokenIndex] = currentTokenIndex;
+        ownedTokenIds[ownedTokenIndex] = currentTokenId;
 
         ownedTokenIndex++;
       }
 
-      currentTokenIndex++;
+      currentTokenId++;
     }
 
-    return ownedTokens;
+    return ownedTokenIds;
   }
 
-  function tokenURI(uint256 tokenId)
+  function tokenURI(uint256 _tokenId)
     public
     view
     virtual
@@ -125,114 +94,72 @@ contract NFT is ERC721, Ownable {
     returns (string memory)
   {
     require(
-      _exists(tokenId),
+      _exists(_tokenId),
       "ERC721Metadata: URI query for nonexistent token"
     );
-    
-    if(revealed == false) {
-      return notRevealedUri;
+
+    if (revealed == false) {
+      return hiddenMetadataUri;
     }
 
     string memory currentBaseURI = _baseURI();
     return bytes(currentBaseURI).length > 0
-        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
+        ? string(abi.encodePacked(currentBaseURI, _tokenId.toString(), uriSuffix))
         : "";
   }
 
-  /**
-   * @dev See {IERC721Enumerable-totalSupply}.
-   */
-  function totalSupply() external view returns (uint256) {
-    return _nextTokenId.current() - 1;
+  function setRevealed(bool _state) public onlyOwner {
+    revealed = _state;
   }
 
-  /**
-   * Override isApprovedForAll to whitelist user's OpenSea proxy accounts to enable gas-less listings.
-   */
-  function isApprovedForAll(address owner, address operator)
-    override
-    public
-    view
-    returns (bool)
-  {
-    // Whitelist OpenSea proxy contract for easy trading.
-    ProxyRegistry proxyRegistry = ProxyRegistry(openSeaProxyRegistryAddress);
-    if (address(proxyRegistry.proxies(owner)) == operator) {
-      return true;
-    }
-
-    return super.isApprovedForAll(owner, operator);
+  function setCost(uint256 _cost) public onlyOwner {
+    cost = _cost;
   }
 
-  /**
-   * This is used instead of msg.sender as transactions won't be sent by the original token owner, but by OpenSea.
-   */
-  function _msgSender()
-    internal
-    override
-    view
-    returns (address sender)
-  {
-    // See: https://github.com/ProjectOpenSea/opensea-creatures/blob/master/contracts/common/meta-transactions/ContentMixin.sol
-    if (msg.sender == address(this)) {
-      bytes memory array = msg.data;
-      uint256 index = msg.data.length;
-      assembly {
-        // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
-        sender := and(
-          mload(add(array, index)),
-          0xffffffffffffffffffffffffffffffffffffffff
-        )
-      }
-    } else {
-      sender = payable(msg.sender);
-    }
-
-    return sender;
+  function setMaxMintAmountPerTx(uint256 _maxMintAmountPerTx) public onlyOwner {
+    maxMintAmountPerTx = _maxMintAmountPerTx;
   }
 
-  //only owner
-  function reveal() public onlyOwner {
-    revealed = true;
-  }
-  
-  function setCost(uint256 _newCost) public onlyOwner {
-    cost = _newCost;
+  function setHiddenMetadataUri(string memory _hiddenMetadataUri) public onlyOwner {
+    hiddenMetadataUri = _hiddenMetadataUri;
   }
 
-  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
-    maxMintAmount = _newmaxMintAmount;
-  }
-  
-  function setNotRevealedURI(string memory _notRevealedURI) public onlyOwner {
-    notRevealedUri = _notRevealedURI;
+  function setUriPrefix(string memory _uriPrefix) public onlyOwner {
+    uriPrefix = _uriPrefix;
   }
 
-  function setBaseURI(string memory _newBaseURI) public onlyOwner {
-    baseURI = _newBaseURI;
+  function setUriSuffix(string memory _uriSuffix) public onlyOwner {
+    uriSuffix = _uriSuffix;
   }
 
-  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
-    baseExtension = _newBaseExtension;
-  }
-
-  function pause(bool _state) public onlyOwner {
+  function setPaused(bool _state) public onlyOwner {
     paused = _state;
   }
- 
-  function withdraw() public payable onlyOwner {
+
+  function withdraw() public onlyOwner {
     // This will pay HashLips 5% of the initial sale.
     // You can remove this if you want, or keep it in to support HashLips and his channel.
     // =============================================================================
     (bool hs, ) = payable(0x943590A42C27D08e3744202c4Ae5eD55c2dE240D).call{value: address(this).balance * 5 / 100}("");
     require(hs);
     // =============================================================================
-    
-    // This will payout the owner 95% of the contract balance.
+
+    // This will transfer the remaining contract balance to the owner.
     // Do not remove this otherwise you will not be able to withdraw the funds.
     // =============================================================================
     (bool os, ) = payable(owner()).call{value: address(this).balance}("");
     require(os);
     // =============================================================================
+  }
+
+  function _mintLoop(address _receiver, uint256 _mintAmount) internal {
+    for (uint256 i = 0; i < _mintAmount; i++) {
+      supply.increment();
+      _safeMint(_receiver, supply.current());
+    }
+  }
+
+  function _baseURI() internal view virtual override returns (string memory) {
+    return uriPrefix;
   }
 }
