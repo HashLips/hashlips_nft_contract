19094663d9d856ef857b8f6d32ea67a2206761cd
Ruben <ruben@bokobza.info>
2022-01-30 17:34:23 +0200

    Fix

--- contract/SimpleNft.sol
+++ contract/SimpleNft.sol
@@ -18,7 +18,7 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFT is ERC721Enumerable, Ownable  {
+contract NFT is ERC721Enumerable, Ownable {
   using Strings for uint256;
 
   string baseURI;
